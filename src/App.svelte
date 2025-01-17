aa6e4435611b8be9b6fc6ada402081cd1b2e9a19
Kresna Satya <kresnasatya72@gmail.com>
2025-01-17 15:54:32 +0800

    refactor: put style inside of component instead of separated files

--- src/App.svelte
+++ src/App.svelte
@@ -17,3 +17,43 @@
     <strong>{numOfCoins}</strong>
   </footer>
 </div>
+
+<style>
+.wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 48px;
+}
+
+.wrapper main {
+  position: relative;
+  display: grid;
+  place-content: center;
+}
+
+.wrapper footer {
+  padding: 16px;
+  font-size: 1.25rem;
+  font-weight: 400;
+  text-align: center;
+}
+
+.wrapper footer strong {
+  display: block;
+  font-size: 2rem;
+}
+
+.floatingNumWrapper {
+  position: absolute;
+  top: -48px;
+  right: 0;
+  padding: 32px;
+  font-size: 3rem;
+  font-weight: bold;
+  color: gold;
+  pointer-events: none;
+  user-select: none;
+}
+</style>
--- src/Coin.svelte
+++ src/Coin.svelte
@@ -1,5 +1,4 @@
 <script>
-    import './Coin.css';
     let { handleNumOfCoins } = $props();
 </script>
 
@@ -9,3 +8,31 @@
         <img class="coin-image" alt="" src="https://sandpack-bundler.vercel.app/img/toonie.png" />
     </button>
 </div>
+
+<style>
+.coin {
+    display: block;
+    background: transparent;
+    border: none;
+    width: 200px;
+    cursor: pointer;
+    transition: transform 500ms;
+    will-change: transform;
+}
+
+.coin:hover {
+    transform: scale(1.05);
+    transition: transform 200ms;
+}
+
+.coin:active {
+    transform: scale(0.95);
+    transition: transform 0ms;
+}
+
+.coin-image {
+    display: block;
+    width: 100%;
+    user-select: none;
+}
+</style>
